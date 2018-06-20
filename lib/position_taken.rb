0de17bf75415e180d90ee56053a6ece36066a738
pchengeducation@hotmail.com <pchengeducation@hotmail.com>
2018-06-20 18:51:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-if board[index] == " "
+if board[0] == " "
   return false
+elsif board[index] == " "
+  return
 end
 end
